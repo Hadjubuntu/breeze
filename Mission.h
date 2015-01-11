540c2e1e8cb15dcda981de8b516861b259792a47
adrien <adrien.hadj.salah@gmail.com>
2015-01-11 18:40:02 +0100

    New nav system

---
+++ Mission.h
@@ -0,0 +1,111 @@
+/*
+ * Mission.h
+
+ *
+ *  Created on: Jan 5, 2015
+ *      Author: Adrien Hadj-Salah
+ */
+// #include <stdint.h>
+#include "Math.h"
+#include "MissionCommon.h"
+#include "Navig2.h"
+
+#ifndef MISSION_H_
+#define MISSION_H_
+
+
+
+
+// Initialization of the mission structure
+//--------------------------------------------------
+void missionInit();
+
+// Insert a new mission element to the list
+//--------------------------------------------------
+void missionAdd(MissionElementType t, MissionElement *e);
+
+// Run the mission to determine which attitude must be commanded
+//--------------------------------------------------
+void missionRun();
+
+
+//--------------------------------------------------
+//--------------------------------------------------
+
+void missionInit() {
+	_mission.currentIdx = 0;
+	_mission.insertIdx = 0;
+	_mission.elementTime = 0.0;
+}
+
+
+void missionAdd(MissionElementType type, MissionElement *element) {
+	uint8_t index = _mission.insertIdx;
+	_mission.elements[index] = (*element);
+
+	_mission.insertIdx ++;
+}
+
+bool missionIsDone() {
+	return (_mission.currentIdx >= _mission.insertIdx) ;
+}
+
+bool missionWPNav(MissionElement *el) {
+	bool elRunning = true;
+
+	if (navCloseEnoughToWP(el->missionWP)) {
+		elRunning = false;
+	}
+	else {
+		navToWP(el->missionWP);
+	}
+
+
+	return elRunning;
+}
+
+bool missionCircleNav(MissionElement *el) {
+	bool elRunning = true;
+	return elRunning;
+}
+
+void missionRun() {
+	long time = micros();
+
+	if (missionIsDone()) {
+
+	}
+	else {
+		bool el_running = false;
+		MissionElement *el = _mission[_mission.currentIdx] ;
+
+		switch (el->type) {
+		case _missionWP:
+			el_running = missionWPNav(el);
+			break;
+		case _missionCircle:
+			el_running = missionCircleNav(el);
+			break;
+		default:
+			// Unknow mission type, therefore, step to next one
+			el_running = true;
+			break;
+		}
+
+		if (lastMissionRunExecUs > 0) {
+			_mission.elementTime += (time - lastMissionRunExecUs) ;
+		}
+
+		// test if element is finished or element time is elapsed
+		if (el_running == false || (el->durationSeconds > 0 && _mission.elementTime >= el->durationSeconds * S_TO_US)) {
+			// reset timer
+			_mission.elementTime = 0.;
+			// go to next element
+			_mission.currentIdx = (_mission.currentIdx + 1) % MISSION_ELEMENT_NB;
+		}
+	}
+
+	lastMissionRunExecUs = time;
+}
+
+#endif /* MISSION_H_ */
---
+++ MissionCommon.h
@@ -0,0 +1,52 @@
+/*
+ * MissionCommon.h
+ *
+ *  Created on: Jan 9, 2015
+ *      Author: adrien
+ */
+
+#ifndef MISSIONCOMMON_H_
+#define MISSIONCOMMON_H_
+
+// Variables
+//--------------------------------------------------
+#define MISSION_ELEMENT_NB 20
+long lastMissionRunExecUs = 0;
+
+enum MissionElementType {
+	_missionWP = 1,
+	_missionCircle = 2
+};
+
+// Structures
+//--------------------------------------------------
+typedef struct T_MISSION_WP {
+	GeoPosition wp;
+} MissionWP ;
+
+typedef struct T_MISSION_CIRCLE {
+	GeoPosition center;
+} MissionCircle ;
+
+typedef struct T_MISSION_ELEMENT {
+
+	long durationSeconds; // = 0, disabled
+	enum MissionElementType type;
+
+	union {
+		MissionWP missionWP;
+		MissionCircle missionCircle;
+	};
+} MissionElement;
+
+typedef struct T_MISSION {
+	MissionElement elements[MISSION_ELEMENT_NB];
+	uint8_t currentIdx; // Current index on the mission
+	uint8_t insertIdx; // Insertion index
+	float elementTime; // Time spend on the current element
+} Mission;
+
+Mission _mission;
+
+
+#endif /* MISSIONCOMMON_H_ */
